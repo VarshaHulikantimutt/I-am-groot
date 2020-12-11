fc8534a1bc3a6949dfd80a86ecf5e3cf1f8aa51d
Varsha Hulikantimutt <VarshaHulikantimutt@users.noreply.github.com>
2020-12-11 11:55:08 +0530

    final commit

---
+++ translate.js
@@ -0,0 +1,30 @@
+var translateButton = document.querySelector("#btn-translate");
+var inputText = document.querySelector("#text-input");
+var outputText = document.querySelector("#text-output");
+
+var serverURL = "https://api.funtranslations.com/translate/groot.json"
+
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("There is an Error ", error);
+    alert("Something went wrong! Please try again after some time.")
+}
+
+function clickHandler(){
+    var text = inputText.value; 
+
+
+    fetch(getTranslationURL(text))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputText.innerText = translatedText;
+           })
+        .catch(errorHandler)
+}
+
+translateButton.addEventListener("click",clickHandler);
